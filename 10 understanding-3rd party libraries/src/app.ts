970ebe494250d8d16a6cc4d2d510c77119bc4183
ugniususc <ugniususc@gmail.com>
2025-01-30 12:16:01 +0200

    find address on google map by input

--- 10 understanding-3rd party libraries/src/app.ts
+++ 10 understanding-3rd party libraries/src/app.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
-const GOOGLE_API_KEY = "AIzaSyBO-ZiAmOppKs5PNR8PLbw_NmNL3ueSppM";
+const GOOGLE_API_KEY = "";
 
 type GoogleGeocodingResponse = {
   results: { geometry: { location: { lat: number; lng: number } } }[];
