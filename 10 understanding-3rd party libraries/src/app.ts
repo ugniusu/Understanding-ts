0d041ab88020ab3a5c49fba8d634dec55c4c4999
ugniususc <ugniususc@gmail.com>
2025-01-30 12:13:15 +0200

    find address in google maps by input

---
+++ 10 understanding-3rd party libraries/src/app.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyBO-ZiAmOppKs5PNR8PLbw_NmNL3ueSppM";
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(e: Event) {
+  e.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((res) => {
+      if (res.data.status !== "OK") {
+        throw new Error("Could not fetch location");
+      }
+      const coordinates = res.data.results[0].geometry.location;
+      const map = new google.maps.Map(
+        document.getElementById("map") as HTMLElement,
+        {
+          center: coordinates,
+          zoom: 16,
+        }
+      );
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+form.addEventListener("submit", searchAddressHandler);
